ca84db5b65cecf177c2c41621ed0a95af6b1ee2c
Eduarda <contatoeduardagaleno@outlook.com>
2025-01-28 10:59:50 -0300

    :wrench: CRUD Categoria :wrench:

---
+++ src/categoria/entities/categoria.entity.ts
@@ -0,0 +1,25 @@
+import { Transform, TransformFnParams } from "class-transformer";
+import { IsNotEmpty } from "class-validator";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+
+@Entity({name: "tb_categorias"})
+export class Categoria {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Transform(({ value }: TransformFnParams) => value?.trim())
+    @IsNotEmpty()
+    @Column({length: 255, nullable: false})
+    titulo: string;
+
+    @Transform(({ value }: TransformFnParams) => value?.trim())
+    @IsNotEmpty()
+    @Column({length: 255, nullable: false})
+    descricao: string;
+
+    @UpdateDateColumn()
+    data: Date;
+
+}
